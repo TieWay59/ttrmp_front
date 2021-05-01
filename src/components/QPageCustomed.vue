70eee74e7a95aa0ba0ee5650acd7f1b526c5ca01
TieWay59 <tieway59@foxmail.com>
2021-05-01 21:17:04 +0800

    增加 QPageCustomed.vue 的上下padding

--- src/components/QPageCustomed.vue
+++ src/components/QPageCustomed.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="fit row wrap justify-center items-start content-start
-      q-gutter-md q-pl-md">
+      q-gutter-md q-pl-md q-py-lg">
     <slot></slot>
   </q-page>
 </template>
